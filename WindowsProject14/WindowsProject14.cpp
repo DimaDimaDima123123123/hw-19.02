023130a0aab2a5c1562b19dedcf406311bc0e5bb
DimaDimaDima123123123 <dmitriyshevel11@gmail.com>
2025-02-23 14:14:11 +0200

    Добавьте файлы проекта.

---
+++ WindowsProject14/WindowsProject14.cpp
@@ -0,0 +1,49 @@
+﻿#include <windows.h>
+#include <tchar.h>
+
+#include "resource.h"
+
+BOOL CALLBACK DlgProc(HWND, UINT, WPARAM, LPARAM);
+
+
+int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPTSTR lpszCmdLine, int nCmdShow)
+{
+
+    return DialogBox(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgProc);
+}
+
+BOOL CALLBACK DlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    TCHAR login[20], password[20];
+
+    int score;
+
+    switch (message)
+    {
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDC_BUTTON2)
+        {
+            GetDlgItemText(hWnd, IDC_EDIT1, login, 30);
+            GetDlgItemText(hWnd, IDC_EDIT2, password, 20);
+
+            if (_tcscmp(login, _T("admin")) == 0 && _tcscmp(password, _T("12345")) == 0)
+            {
+                MessageBox(hWnd, _T("Admin"), _T("Message"), MB_OK | MB_ICONINFORMATION);
+            }
+
+            else
+            {
+                MessageBox(hWnd, _T("Wrong"), _T("Error"), MB_OK | MB_ICONERROR);
+            }
+        }
+
+    case WM_SHOWWINDOW:
+
+        return TRUE;
+    case WM_CLOSE:
+
+        EndDialog(hWnd, 0);
+        return TRUE;
+    }
+    return FALSE;
+}
